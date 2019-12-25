3cb723aa479e8fd73c39f6dfdd422c2bfbb9b442
mobarak25 <mobarakali62@gmail.com>
2019-12-25 21:42:07 +0600

    pos

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="">
+        <Header />
+        <section class="site-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-2 pl-0">
+                        <div class="sidebar">
+                            <Sidebar />
+                        </div>
+                    </div>
+                    <div class="col-lg-10">
+                        <div class="home">
+                            <router-view/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        
+        
+        <Footer />
+    </div>
+</template>
+
+<script>
+    import Header from '@/components/Header.vue'
+    import Footer from '@/components/Footer.vue'
+    import Sidebar from '@/components/Sidebar.vue'
+
+    export default {
+        name: '',
+        components: {
+            Header,
+            Footer,
+            Sidebar
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="home-cont">
+		<h1>This text come form home page</h1>
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+	name: 'home',
+	components: {}
+}
+</script>
