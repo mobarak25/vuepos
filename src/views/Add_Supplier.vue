16509dc96566c1efb803d118667e2fedb1102302
mobarakweb <mobarakali62@gmail.com>
2020-01-18 10:49:14 +0600

    pos

--- src/views/Add_Supplier.vue
+++ src/views/Add_Supplier.vue
@@ -10,67 +10,68 @@
         <span class="sr-only">Loading...</span>
       </div>
     </div>
+    <div v-else>
+      <form v-on:submit.prevent="submitData">
+        <div class="row form-group">
+          <label class="col-sm-2 col-form-label">Supplier ID:</label>
+          <div class="col-sm-4">
+            <input :value="supplierID" type="text" class="form-control" readonly />
+          </div>
+          <label class="col-sm-2 col-form-label">Supplier Name:</label>
+          <div class="col-sm-4">
+            <input ref="supplirName" v-model="collects.supplirName" type="text" class="form-control" />
+          </div>
+        </div>
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Address:</label>
+          <div class="col-sm-4">
+            <textarea ref="address" v-model="collects.address" class="form-control" rows="2"></textarea>
+          </div>
+          <label class="col-sm-2 col-form-label">Remarks:</label>
+          <div class="col-sm-4">
+            <textarea ref="remarks" v-model="collects.remarks" class="form-control" rows="2"></textarea>
+          </div>
+        </div>
 
-    <form v-else v-on:submit.prevent="submitData">
-      <div class="row form-group">
-        <label class="col-sm-2 col-form-label">Supplier ID:</label>
-        <div class="col-sm-4">
-          <input :value="supplierID" type="text" class="form-control" readonly />
+        <div class="form-group row">
+          <label class="col-sm-2 col-form-label">Contact No:</label>
+          <div class="col-sm-4">
+            <input ref="contactNo" v-model="collects.contactNo" type="text" class="form-control" />
+          </div>
+          <div class="col-sm-2"></div>
+          <div class="col-lg-4">
+            <button type="submit" class="btn btn-dark mb-3">Save Supplier</button>
+          </div>
         </div>
-        <label class="col-sm-2 col-form-label">Supplier Name:</label>
-        <div class="col-sm-4">
-          <input ref="supplirName" v-model="collects.supplirName" type="text" class="form-control" />
-        </div>
-      </div>
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Address:</label>
-        <div class="col-sm-4">
-          <textarea ref="address" v-model="collects.address" class="form-control" rows="2"></textarea>
-        </div>
-        <label class="col-sm-2 col-form-label">Remarks:</label>
-        <div class="col-sm-4">
-          <textarea ref="remarks" v-model="collects.remarks" class="form-control" rows="2"></textarea>
-        </div>
-      </div>
+      </form>
+      <table class="table table-hover table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Contact No.</th>
+            <th>Remarks</th>
+            <th class="text-center">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(supplier,index) in jsonData" :key="index">
+            <td v-text="supplier.id"></td>
+            <td v-text="supplier.name"></td>
+            <td v-text="supplier.address"></td>
+            <td v-text="supplier.contact_no"></td>
+            <td v-text="supplier.remarks"></td>
 
-      <div class="form-group row">
-        <label class="col-sm-2 col-form-label">Contact No:</label>
-        <div class="col-sm-4">
-          <input ref="contactNo" v-model="collects.contactNo" type="text" class="form-control" />
-        </div>
-        <div class="col-sm-2"></div>
-        <div class="col-lg-4">
-          <button type="submit" class="btn btn-dark mb-3">Save Supplier</button>
-        </div>
-      </div>
-    </form>
-    <table class="table table-hover table-bordered table-striped">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Address</th>
-          <th>Contact No.</th>
-          <th>Remarks</th>
-          <th class="text-center">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(supplier,index) in jsonData" :key="index">
-          <td v-text="supplier.id"></td>
-          <td v-text="supplier.name"></td>
-          <td v-text="supplier.address"></td>
-          <td v-text="supplier.contact_no"></td>
-          <td v-text="supplier.remarks"></td>
-
-          <td class="text-center">
-            <button @click="deleteSupplier(supplier.id)" class="btn btn-sm btn-danger ml-2">
-              <i class="fa fa-trash"></i>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            <td class="text-center">
+              <button @click="deleteSupplier(supplier.id)" class="btn btn-sm btn-danger ml-2">
+                <i class="fa fa-trash"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -91,7 +92,7 @@ export default {
       title: "Suppliers Entry",
       collects: {},
       supplierID: "",
-      showSpinner: false,
+      showSpinner: true,
       jsonData: null,
       host: "https://vuepos.000webhostapp.com/pos"
     };
@@ -134,6 +135,7 @@ export default {
       let url = this.host + "/get_suppliers.php";
       axios.get(url).then(res => {
         this.jsonData = res.data;
+          this.showSpinner = false;
       });
     },
 
